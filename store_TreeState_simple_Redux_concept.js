a87454495c2cef3cb99ceb5b171fc935d7e7d07d
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2022-02-17 00:39:23 +0200

    Support Multi Reducers in store.

--- store_TreeState_simple_Redux_concept.js
+++ store_TreeState_simple_Redux_concept.js
@@ -81,16 +81,22 @@ function createStore(reducer) {
 }
 
 
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
 function todos(state = [], action) {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return state.concat([action.todo]);
 
-    case 'REMOVE_TODO':
+    case REMOVE_TODO:
 
       return state.filter((s) => s.id !== action.id);
 
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
 
       return state.map(
           (todo) => todo.id === action.id ?
@@ -101,7 +107,26 @@ function todos(state = [], action) {
   }
 }
 
-const store = createStore(todos)
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+
+    case REMOVE_GOAL:
+
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action), goals: goals(state.goals, action)
+  }
+}
+
+const store = createStore(app)
 
 
 store.subscribe(() => {console.log('The new state is: ', store.getState())})
